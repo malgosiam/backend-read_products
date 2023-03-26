30bebdd1d0368db3062412de6a89857ccbdca8be
Malgorzata Wawrzyniak <malgosia.archi@gmail.com>
2023-03-26 13:52:30 +0200

    refactor code and add data from local MongoDB

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,13 +1,14 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
-
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    response.status(200).json(product);
+  } else {
+    return response.status(404).json({ Status: "Not Found" });
   }
-
-  response.status(200).json(product);
 }
